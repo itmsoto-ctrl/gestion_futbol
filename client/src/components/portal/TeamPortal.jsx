68ea876c557f66731fd99251547fb31ca6b4b3ec
Daniel Martínez <levertouch@iMac-de-Daniel.local>
2026-03-02 17:25:55 +0100

    fix: ajuste quirúrgico de los puntos de import

--- client/src/components/portal/TeamPortal.jsx
+++ client/src/components/portal/TeamPortal.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import API_BASE_URL from '../../../apiConfig';
+import API_BASE_URL from '../../apiConfig';
 
 const TeamPortal = () => {
   const { token } = useParams();
